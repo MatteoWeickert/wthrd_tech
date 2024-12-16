17bac81b32844021d06ce149a2d2ba2ae632f192
mdinnus <mdinnus@uni-muenster.de>
2024-12-16 22:06:42 +0100

    Enabled filtering on all parameters, changed view of models

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ async function fetchItems() {
         const data = await response.json();
 
         if (Array.isArray(data) && data.length > 0) {
-            displayItems(data);
+            displayItems(data, undefined);
             console.log(printAllFilters(data));
         } else {
             showAlert(4, "Fehler beim Abrufen der Items.", "Überprüfe die Netzwerkverbindung.")
@@ -30,7 +30,7 @@ function printAllFilters(items) {
     let filterContent = '';
     const sidebar = document.getElementById("sidebar");
 
-    sidebar.innerHTML = ''; 
+    sidebar.innerHTML = ''; // Vorherige Inhalte der Sidebar löschen
 
     const toggleButton = `
         <button class="d-md-none position-absolute top-0 end-0 m-2 btn btn-link p-0" type="button" data-bs-toggle="collapse" data-bs-target="#sidebar" aria-controls="sidebar" aria-expanded="false" aria-label="Toggle Sidebar">
@@ -40,7 +40,7 @@ function printAllFilters(items) {
         </button>
     `;
 
-    let selectedFilters = {}; 
+    let selectedFilters = []; // Zur Speicherung der ausgewählten Filter
 
     Object.keys(filters).forEach(group => {
         let options = '';
@@ -48,7 +48,7 @@ function printAllFilters(items) {
             const optionId = `filter-${group}-${option.replace(/[^a-zA-Z0-9-_]/g, '')}`;
             options += `
                 <div class="form-check form-check-inline">
-                    <input type="checkbox" class="form-check-input" id="${optionId}" value="${option}">
+                    <input type="checkbox" class="form-check-input" id="${optionId}" value="${option}" data-group="${group}">
                     <label class="form-check-label" for="${optionId}">${option}</label>
                 </div>
             `;
@@ -77,14 +77,16 @@ function printAllFilters(items) {
     const checkboxes = sidebar.querySelectorAll('.form-check-input');
     checkboxes.forEach(checkbox => {
         checkbox.addEventListener('change', () => {
-            const group = checkbox.closest('.form-check').previousElementSibling.textContent.trim();
+            const group = checkbox.dataset.group;
             const value = checkbox.value;
 
             if (checkbox.checked) {
                 if (!selectedFilters[group]) {
                     selectedFilters[group] = [];
                 }
-                selectedFilters[group].push(value);
+                if (!selectedFilters[group].includes(value)) {
+                    selectedFilters[group].push(value);
+                }
             } else {
                 if (selectedFilters[group]) {
                     selectedFilters[group] = selectedFilters[group].filter(item => item !== value);
@@ -94,88 +96,179 @@ function printAllFilters(items) {
                 }
             }
 
-            console.log(selectedFilters);
+            displayItems(items, selectedFilters);
         });
     });
 }
 
+// Items anhand der angekreuzten Filterparameter filtern
+function filterItems(items, filters){
+    showAlert(0)
+    let selectedItems = [];
+    let matchingValues = true;
+    if(!filters || Object.keys(filters).length === 0){
+        return items
+    }
+    else{
+        items.forEach( item=> {
+                //Framework filtern
+                if(filters.frameworks && Object.keys(filters.frameworks).length > 0){
+                    //console.log("frameworks")
+                    if(item.properties['mlm:framework'].includes(filters.frameworks)){
+                        // console.log("frameworks accessed")
+                        matchingValues = true;
+                    }
+                    else{
+                        return;
+                    }
+                }
+                //Accelerators filtern
+                if(filters.accelerators && Object.keys(filters.accelerators).length > 0){
+                    //console.log("accelerator")
+                    if(item.properties['mlm:accelerator'].includes(filters.accelerators)){
+                        //console.log("accelerator accessed")
+                        matchingValues = true;
+                    }
+                    else{
+                        return;
+                    }
+                }
+                // Tasks filtern
+                if (filters.tasks && Object.keys(filters.tasks).length > 0){
+                    // console.log("tasks")
+                    // console.log(JSON.stringify(filters.tasks))
+                    // console.log(JSON.stringify(item.properties['mlm:tasks']))
+                    if (Array.from(item.properties['mlm:tasks']).some(task => filters.tasks.includes(task))) {
+                        //console.log("tasks accessed")
+                        matchingValues = true;
+                    } else {
+                        return;
+                    }
+                }
+                //Inputs filtern
+                if(filters.inputTypes && Object.keys(filters.inputTypes).length > 0){
+                    // console.log("input")
+                    // console.log(item.properties['mlm:input'].type.includes(filters.inputTypes))
+                    if(item.properties['mlm:input'].type.includes(filters.inputTypes)){
+                        // console.log("input accessed")
+                        matchingValues = true;
+                    }
+                    else{
+                        return;
+                    }
+                }
+
+                // Pretrained Sources
+                if (filters.pretrainedSources && item.properties['mlm:pretrained_source'] !== 'None' && Array.isArray(filters.pretrainedSources) && filters.pretrainedSources.length > 0) {
+                    if (filters.pretrainedSources.includes(item.properties['mlm:pretrained_source'])) {
+                        // console.log("Pretrained sources accessed");
+                        matchingValues = true;
+                    } else {
+                        return;
+                    }
+                }
+
+                // Accelerator Summary filtern
+                if (filters.acceleratorSummaries && item.properties['mlm:accelerator_summary'] !== 'None' && Array.isArray(filters.acceleratorSummaries) && filters.acceleratorSummaries.length > 0) {
+                    // console.log(filters.acceleratorSummaries);
+                    // console.log(item.properties['mlm:accelerator_summary']);
+                    if (filters.acceleratorSummaries.includes(item.properties['mlm:accelerator_summary'])) {
+                        // console.log("Accelerator summary accessed");
+                        matchingValues = true;
+                    } else {
+                        return;
+                    }
+                }
+                if(matchingValues){
+                    selectedItems.push(item)
+                }
+        });
+        if(Object.keys(selectedItems).length == 0){
+            showAlert(2, "Keine Modelle gefunden.", "Nutze andere Suchparameter oder füge ein weiteres Modell mit deinen Anforderungen hinzu.")
+        }
+        console.log(selectedItems);
+        return selectedItems;
+    }
+}
+
 // Funktion zum Anzeigen aller Modelle
-function displayItems(items) {
+function displayItems(items, filters) {
     const container = document.getElementById('modell-container');
     container.innerHTML = '';
+    const filteredItems = filterItems(items, filters);
 
-    items.forEach(item => { 
-        const itemDiv = document.createElement('div');
-        itemDiv.classList.add('p-3', 'modell-item');
+    filteredItems.forEach(item => { 
+                const itemDiv = document.createElement('div');
+                itemDiv.classList.add('p-3', 'modell-item');
 
-        const title = document.createElement('span');
-        title.innerHTML = `${item.properties.title || 'Unbekannter Titel'}`;
+                const title = document.createElement('span');
+                title.innerHTML = `${item.properties.title || 'Unbekannter Titel'}`;
 
-        const parameters = document.createElement('div');
-        parameters.classList.add('modell-itemparameter');
-        parameters.id = `modell-itemparameter-${item.id}`;
-        parameters.innerHTML = `
-            ${decideOnParameters(item)}
-            <button type="button" 
-                    class="btn-expand" 
-                    data-bs-toggle="collapse" 
-                    data-bs-target="#collapse-${item.id}" 
-                    aria-expanded="false" 
-                    aria-controls="collapse-${item.id}">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#1C3D86" class="bi bi-caret-down" viewBox="0 0 16 16">
-                        <path d="M3.204 5h9.592L8 10.481zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659"/>
-                    </svg>
-            </button>
-        `;
+                const parameters = document.createElement('div');
+                parameters.classList.add('modell-itemparameter');
+                parameters.id = `modell-itemparameter-${item.id}`;
+                parameters.innerHTML = `
+                    ${fillInParameters(item, filters)}
+                    <button type="button" 
+                            class="btn-expand" 
+                            data-bs-toggle="collapse" 
+                            data-bs-target="#collapse-${item.id}" 
+                            aria-expanded="false" 
+                            aria-controls="collapse-${item.id}">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#1C3D86" class="bi bi-caret-down" viewBox="0 0 16 16">
+                                <path d="M3.204 5h9.592L8 10.481zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659"/>
+                            </svg>
+                    </button>
+                `;
 
-        const information = document.createElement('div');
-        information.id = 'modell-itemcollapse'
-        information.innerHTML = `
-            <div class="collapse" id="collapse-${item.id}">
-                <div class="card card-body">
-                    <div class="card-body-title">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-braces" viewBox="0 0 16 16">
-                            <path d="M2.114 8.063V7.9c1.005-.102 1.497-.615 1.497-1.6V4.503c0-1.094.39-1.538 1.354-1.538h.273V2h-.376C3.25 2 2.49 2.759 2.49 4.352v1.524c0 1.094-.376 1.456-1.49 1.456v1.299c1.114 0 1.49.362 1.49 1.456v1.524c0 1.593.759 2.352 2.372 2.352h.376v-.964h-.273c-.964 0-1.354-.444-1.354-1.538V9.663c0-.984-.492-1.497-1.497-1.6M13.886 7.9v.163c-1.005.103-1.497.616-1.497 1.6v1.798c0 1.094-.39 1.538-1.354 1.538h-.273v.964h.376c1.613 0 2.372-.759 2.372-2.352v-1.524c0-1.094.376-1.456 1.49-1.456V7.332c-1.114 0-1.49-.362-1.49-1.456V4.352C13.51 2.759 12.75 2 11.138 2h-.376v.964h.273c.964 0 1.354.444 1.354 1.538V6.3c0 .984.492 1.497 1.497 1.6"/>
-                        </svg>
-                        <span style="font-size: 10px;">${item.properties['mlm:name']} /   </span><span style="font-size:20px;">${item.properties.title}</span>
-                        <img src="${item.assets.data['thumbnail']}" width="10px" height="10px" alt="" />
-                    </div>
-                    <hr>
-                    <span style="font-size:15px;">Beschreibung:</span>
-                    <span style="font-size:12px;">${item.properties.description}</span>
-                    <hr>
-                    <div class="card-body-download">
-                        <span style="font-size:15px;">Einbinden:</span><br> <span style="font-size:12px;">${item.assets.data['href']}</span>
-                            <button type="button" class="btn-clipboard" onclick="copyToClipboard('${item.assets.data['href']}', '${item.properties['mlm:name']}')" id="clipboard-${item.id}">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#414243" class="bi bi-clipboard" viewBox="0 0 16 16">
-                                    <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z"/>
-                                    <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0z"/>
+                const information = document.createElement('div');
+                information.id = 'modell-itemcollapse'
+                information.innerHTML = `
+                    <div class="collapse" id="collapse-${item.id}">
+                        <div class="card card-body">
+                            <div class="card-body-title">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-braces" viewBox="0 0 16 16">
+                                    <path d="M2.114 8.063V7.9c1.005-.102 1.497-.615 1.497-1.6V4.503c0-1.094.39-1.538 1.354-1.538h.273V2h-.376C3.25 2 2.49 2.759 2.49 4.352v1.524c0 1.094-.376 1.456-1.49 1.456v1.299c1.114 0 1.49.362 1.49 1.456v1.524c0 1.593.759 2.352 2.372 2.352h.376v-.964h-.273c-.964 0-1.354-.444-1.354-1.538V9.663c0-.984-.492-1.497-1.497-1.6M13.886 7.9v.163c-1.005.103-1.497.616-1.497 1.6v1.798c0 1.094-.39 1.538-1.354 1.538h-.273v.964h.376c1.613 0 2.372-.759 2.372-2.352v-1.524c0-1.094.376-1.456 1.49-1.456V7.332c-1.114 0-1.49-.362-1.49-1.456V4.352C13.51 2.759 12.75 2 11.138 2h-.376v.964h.273c.964 0 1.354.444 1.354 1.538V6.3c0 .984.492 1.497 1.497 1.6"/>
                                 </svg>
-                            </button>
+                                <span style="font-size: 10px;">${item.properties['mlm:name']} /   </span><span style="font-size:20px;">${item.properties.title}</span>
+                                <img src="${item.assets.data['thumbnail']}" width="10px" height="10px" alt="" />
+                            </div>
+                            <hr>
+                            <span style="font-size:15px;">Beschreibung:</span>
+                            <span style="font-size:12px;">${item.properties.description}</span>
+                            <hr>
+                            <div class="card-body-download">
+                                <span style="font-size:15px;">Einbinden:</span><br> <span style="font-size:12px;">${item.assets.data['href']}</span>
+                                    <button type="button" class="btn-clipboard" onclick="copyToClipboard('${item.assets.data['href']}', '${item.properties['mlm:name']}')" id="clipboard-${item.id}">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#414243" class="bi bi-clipboard" viewBox="0 0 16 16">
+                                            <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z"/>
+                                            <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0z"/>
+                                        </svg>
+                                    </button>
+                            </div>
+                            <hr>
+                            <div style="font-size:12px;"class="card-body-parameters">
+                                <span>Tasks: ${item.properties['mlm:accelerator'] || 'Unbekannt'} </span><br>
+                                <span>Modell: ${item.properties['mlm:tasks']}</span><br>
+                                <span>Framework: ${item.properties['mlm:framework'] || 'Unbekannt'} </span><br>
+                                <span>Link: ${item.assets.data['href'] || 'Unbekannt'} </span>
+                                <hr>
+                            </div>
+                            <span style="font-size:10px;">Letztes Update: ${item['updated_at'] || 'Unbekannt'} </span>
+                        </div>
                     </div>
-                    <hr>
-                    <div style="font-size:12px;"class="card-body-parameters">
-                        <span>Tasks: ${item.properties['mlm:tasks'] || 'Unbekannt'} </span><br>
-                        <span>Modell: ${item.properties['mlm:name']}</span><br>
-                        <span>Framework: ${item.properties['mlm:framework'] || 'Unbekannt'} </span><br>
-                        <span>Link: ${item.assets.data['href'] || 'Unbekannt'} </span>
-                        <hr>
-                    </div>
-                    <span style="font-size:10px;">Letztes Update: ${item['updated_at'] || 'Unbekannt'} </span>
-                </div>
-            </div>
-        `;
+                `;
 
-        itemDiv.appendChild(title);
-        itemDiv.appendChild(parameters);
-        itemDiv.appendChild(information);
-        container.appendChild(itemDiv);
-    });
+                itemDiv.appendChild(title);
+                itemDiv.appendChild(parameters);
+                itemDiv.appendChild(information);
+                container.appendChild(itemDiv);
+        });
 }
 
 // Funktion zum kopieren von Informationen in die Zwischenablage
 function copyToClipboard(url_text, model_name) {
-    console.log("reached");
+    //console.log("reached");
     navigator.clipboard.writeText(url_text).then(() => {
         showAlert(2, `Link des Modells <i>${model_name}</i> erfolgreich in die Zwischenablage kopiert.`, "");
     }).catch(err => {
@@ -184,19 +277,26 @@ function copyToClipboard(url_text, model_name) {
 }
 
 // Funktion um anzuzeigende Informationen zu den Modellen zu generieren
-function decideOnParameters(item){
-    //filters = getCurrentFilters();
-    return `           <span>
-                ${item.properties['mlm:tasks'] || ''} - 
-                ${item.properties['mlm:framework'] || ''} - 
-                ${item.assets.data['href'] || 'Kein Link'}
-            </span>`     
+function fillInParameters(item, filters){
+    let parameter1 = "1";
+    let parameter2 = "2";
+    let parameter3 = "3";
+    // chooseParameters();
+        return `           <span>
+                    ${parameter1} - 
+                    ${parameter2} - 
+                    ${parameter3}
+                </span>`  
+    // }
 }
 
 // Funktion zum erstellen von dynmaischen Alerts 1 Warnung 2 Info 3 Erfolg 4 Error
 function showAlert(type, text, optional){
     alertContainer = document.getElementById('main-alert')
     switch(type){
+        case(0): // CLEAN
+            alertContainer.innerHTML = ``
+        break;
         case(1): // WARNING
             alertContainer.innerHTML = `                            <div class="alert alert-warning alert-dismissible fade show" role="alert">
                                 <strong>Warnung:</strong> ${text} ${optional}
