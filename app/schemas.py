9a6eacf826a03239c363a118a4032e41e1eb382c
mdinnus <mdinnus@uni-muenster.de>
2025-01-21 18:39:54 +0100

    Schemas angepasst => Farbe ist notwendig

--- app/schemas.py
+++ app/schemas.py
@@ -35,7 +35,7 @@ class ItemCreate(BaseModel):
     collection_id: str
     created_at: datetime
     updated_at: datetime
-    color: str = Field(None, description="The given HEX color code is invalid.")
+    color: str = Field(..., description="The given HEX color code is invalid.")
 
     @validator("geometry")
     def validate_geometry(cls, value):
