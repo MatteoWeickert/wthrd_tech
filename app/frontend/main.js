a27b99f922066de5d1208beb351b50dc30aa4f06
mdinnus <mdinnus@uni-muenster.de>
2025-02-02 15:25:14 +0100

    Added userinformation to every model added on page

--- app/frontend/main.js
+++ app/frontend/main.js
@@ -730,6 +730,7 @@ async function fetchItems() {
 async function addItems() {
     const input = getUserInputs();
     const token = sessionStorage.getItem("token");
+    const userdata = await getAuthData();
     try {
         const response = await fetch('http://localhost:8000/addItem/', {
             method: 'POST',
@@ -787,12 +788,15 @@ async function addItems() {
                     data: { href: "https://example.com/data" }
                 },
                 collection_id: input.collectionid,
+                creator_id: userdata.id,
+                creator_username: userdata.username,
                 created_at: new Date().toISOString(),
                 updated_at: new Date().toISOString(),
                 color: getSelectedColor()
             })
         });
         const data = await response.json();
+        console.log(data)
 
         // Verarbeiten der Nachricht aus der API-Antwort
         if (data.message === "Item added successfully") {
@@ -2009,11 +2013,21 @@ function displayItems(items, filters) {
             information.innerHTML = `
                 <div class="collapse" id="collapse-${item.id}">
                     <div class="card card-body">
-                        <div class="card-body-title">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-braces" viewBox="0 0 16 16">
-                                <path d="M2.114 8.063V7.9c1.005-.102 1.497-.615 1.497-1.6V4.503c0-1.094.39-1.538 1.354-1.538h.273V2h-.376C3.25 2 2.49 2.759 2.49 4.352v1.524c0 1.094-.376 1.456-1.49 1.456v1.299c1.114 0 1.49.362 1.49 1.456v1.524c0 1.593.759 2.352 2.372 2.352h.376v-.964h-.273c-.964 0-1.354-.444-1.354-1.538V9.663c0-.984-.492-1.497-1.497-1.6M13.886 7.9v.163c-1.005.103-1.497.616-1.497 1.6v1.798c0 1.094-.39 1.538-1.354 1.538h-.273v.964h.376c1.613 0 2.372-.759 2.372-2.352v-1.524c0-1.094.376-1.456 1.49-1.456V7.332c-1.114 0-1.49-.362-1.49-1.456V4.352C13.51 2.759 12.75 2 11.138 2h-.376v.964h.273c.964 0 1.354.444 1.354 1.538V6.3c0 .984.492 1.497 1.497 1.6"/>
-                            </svg>
-                            <span style="font-size: 10px;">${item.collection_id} /   </span><span style="font-size:20px; color:${item.color}">${item.properties['mlm:name']}</span>
+                        <div class="card-body-title" style="display: flex; justify-content: space-between; align-items: center;">
+                            <div>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-braces" viewBox="0 0 16 16">
+                                    <path d="M2.114 8.063V7.9c1.005-.102 1.497-.615 1.497-1.6V4.503c0-1.094.39-1.538 1.354-1.538h.273V2h-.376C3.25 2 2.49 2.759 2.49 4.352v1.524c0 1.094-.376 1.456-1.49 1.456v1.299c1.114 0 1.49.362 1.49 1.456v1.524c0 1.593.759 2.352 2.372 2.352h.376v-.964h-.273c-.964 0-1.354-.444-1.354-1.538V9.663c0-.984-.492-1.497-1.497-1.6M13.886 7.9v.163c-1.005.103-1.497.616-1.497 1.6v1.798c0 1.094-.39 1.538-1.354 1.538h-.273v.964h.376c1.613 0 2.372-.759 2.372-2.352v-1.524c0-1.094.376-1.456 1.49-1.456V7.332c-1.114 0-1.49-.362-1.49-1.456V4.352C13.51 2.759 12.75 2 11.138 2h-.376v.964h.273c.964 0 1.354.444 1.354 1.538V6.3c0 .984.492 1.497 1.497 1.6"/>
+                                </svg>
+                                <span style="font-size: 10px;">${item.collection_id} / </span>
+                                <span style="font-size:20px; color:${item.color}">${item.properties['mlm:name']}</span>
+                            </div>
+                            <div style="text-align: right; font-size: 10px;">
+                                Erstellt von:
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                                </svg>
+                                ${item.creator_username}
+                            </div>
                         </div>
                         <hr>
                         <div class="card-body-download">
